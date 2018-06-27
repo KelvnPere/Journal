c936eb8d41dd128c282825dc7587d0205f06c596
Pheomix73 <Perekelvin66@gmail.com>
2018-06-27 15:09:32 +0100

    My First Andela 7 Days Coding Challenege Commit

---
+++ app/src/main/java/com/udacity/andela/journal/SignInActivity.java
@@ -0,0 +1,32 @@
+package com.udacity.andela.journal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignInActivity extends AppCompatActivity {
+    // Declaring an instance of FirebaseAuth
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    //Initializing the FirebaseAuth
+    mAuth = FirebaseAuth.getInstance();
+    // When initializing the activity, i need to check to see if the user is currently signed in.
+        @Override
+        public void onStart() {
+            super.onStart();
+            // Check if user is signed in (non-null) and update UI accordingly.
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            updateUI(currentUser);
+
+
+        }
+
+
+    }
+}
